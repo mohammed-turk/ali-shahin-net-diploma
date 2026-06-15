400d5f3d7ff81081c8491b4a1b61cd646993695c
mohammed-rurk <mtj6611@gmail.com>
2026-06-15 18:42:12 +0300

    feet(Referance VS Value types): complete section and excercises

--- 01-CSBasics/Variables/Program.cs
+++ 01-CSBasics/Variables/Program.cs
@@ -2,8 +2,11 @@
 {
     internal class Program
     {
+        int x = 5; // this variable is accessible within the entire class
         static void Main(string[] args)
         {
+            int x = 5; // this variable is only accessible within this method, it is a local variable
+
             // built in Variables
 
             //numeric
@@ -48,25 +51,83 @@
             Console.WriteLine(ulong.MaxValue);
 
             Console.WriteLine("------ float ---------");
-            float num9 = 5;
+            float num9 = 5f;
             Console.WriteLine(float.MinValue);
             Console.WriteLine(float.MaxValue);
 
             Console.WriteLine("------ double ---------");
-            double num10 = 5;
+            double num10 = 5d;
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
 
             Console.WriteLine("------ decimal ---------");
-            decimal num11 = 5;
+            decimal num11 = 5m;
             Console.WriteLine(decimal.MinValue);
             Console.WriteLine(decimal.MaxValue);
             
             // logical
+            bool isTrue = true;
             //char
+            char char1 = 'A';
             // strings
+            string str1 = "Hello World";
             // datatime
+            DateTime date1 = DateTime.Now;
             //object
+            object obj1 = new object();
+
+            // start with these for variables naming and capatalized the next words:
+            // n for numbers
+            // b for boolean
+            // c for char
+            // s for string
+            // dt for datetime
+            // o for object
+
+            // implicitly typed variable, the compiler will determine the type based on the value assigned to it
+            var num12 = 5; // this will be an int, type determined with first initalization, after assigning a value, you can't change its type.
+            // var cannot be sent as method parameters while the object can be.
+            scoping();
+
+            // var can be initalized as annonymous user defined datatype.
+            var myVar = new { Name = "John", Age = 30 }; // this is an anonymous type with two properties: Name and Age
+            Console.WriteLine(myVar.Name); // output: John
+            Console.WriteLine(myVar.Age); // output: 30
+            // myVar.Name = "Jane"; // error: cannot modify the properties of an anonymous type, they are read-only
+
+            ValueType();
+            ReferanceType();
+
+
+        }
+
+        static void scoping()
+        {
+            int x = 5; // this variable is only accessible within this method
+            if (x > 0)
+            {
+                int y = 10; // this variable is only accessible within this block
+                Console.WriteLine(x + y); // can access both x and y here
+            }
+            // Console.WriteLine(y); // error: y is not accessible here
+        }
+
+        static void ValueType()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            num1 = num2;
+            num1 = 3;
+            Console.WriteLine(num1); // output: 3
+            Console.WriteLine(num2);
+        }
+
+        static void ReferanceType()
+        {
+            int[] arr1 = new int[] { 1, 2, 3 };
+            int[] arr2 = arr1; // arr2 is now referencing the same array as arr1
+            arr1[0] = 10; // changing the first element of the array through arr1
+            Console.WriteLine(arr2[0]); // output: 10, because arr2 is referencing the same array as arr1
         }
     }
 }
