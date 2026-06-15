31e030ee0d46a3e77f7b128650ad3d6beb2382e7
mtj66 <mtj6611@gmail.com>
2026-06-15 05:32:06 +0300

    Solution and project exploring

---
+++ 01-CSBasics/GetStarted/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GetStarted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
