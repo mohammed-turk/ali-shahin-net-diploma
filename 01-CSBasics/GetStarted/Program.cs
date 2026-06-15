fc495737a1da8f8d917cb92a6e478a261925468f
mohammed-rurk <mtj6611@gmail.com>
2026-06-15 08:58:57 +0300

    Finalizing the namespace, claasses, methods, and the usings concepts

--- 01-CSBasics/GetStarted/Program.cs
+++ 01-CSBasics/GetStarted/Program.cs
@@ -1,11 +1,24 @@
 ﻿namespace GetStarted
 {
+    namespace SubNamespace1
+    {
+        public class Class1
+        {
+            public void Method1()
+            {
+                Console.WriteLine("Hello from SubNamespace1.Class1.Method1");
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.ReadKey();
+            GetStarted.SubNamespace1.Class1 obj = new GetStarted.SubNamespace1.Class1();
+            Console.WriteLine();
+            obj.Method1();
         }
     }
 }
