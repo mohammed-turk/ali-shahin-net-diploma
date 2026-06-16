3c12c06fcbb22da764a8a092fcec1748ffb4e840
mohammed-rurk <mtj6611@gmail.com>
2026-06-16 05:36:59 +0300

    feet(YearsToDays): complete and test the feature

---
+++ 01-CSBasics/Variables/SwabTwoNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class SwabTwoNumbers
+    {
+
+        static public void SwabTwoNumbersMethod()
+        {
+            Console.WriteLine("Enter the first number:");
+            int nFirstNumber = 0;
+            bool isValidFirstNumber = int.TryParse(Console.ReadLine(), out nFirstNumber);
+            int nSecondNumber = 0;
+           Console.WriteLine("Enter the second number:");
+            bool isValidSecondNumber =  int.TryParse(Console.ReadLine(), out nSecondNumber);
+
+            int nTemp = 0;
+            nTemp = nFirstNumber;
+            nFirstNumber = nSecondNumber;
+            nSecondNumber = nTemp;
+
+            Console.WriteLine("After swabbing the two numbers: first is " + nFirstNumber + " and second is " + nSecondNumber);
+           
+
+
+
+
+        }
+    }
+}
---
+++ 01-CSBasics/Variables/YearsToDays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class YearsToDays
+    {
+        static public void ConvertYearsToDays()
+        {
+            Console.WriteLine("Enter the number of years:");
+            int nYears = 0;
+            bool isValidYears = int.TryParse(Console.ReadLine(), out nYears);
+                        if (!isValidYears)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number for the years.");
+            }
+            else if (nYears < 0)
+            {
+                Console.WriteLine("Years cannot be negative. Please enter a non-negative number.");
+            }
+            else
+            {
+                var yearsCalculations = new { 
+                 nMonths = nYears * 12,
+                 nWeaks = nYears * 52,
+                 nHours = nYears * 365 * 24,
+                 nDays = nYears * 365
+                };
+                 // This does not account for leap years, but it's a common approximation.
+                Console.WriteLine($"{nYears} years is {yearsCalculations.nMonths} months, {yearsCalculations.nWeaks} weeks, and {yearsCalculations.nDays} days.");
+            }
+        }
+    }
+}
