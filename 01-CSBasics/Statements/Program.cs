f56e6feb770aa8971a6d689638331944960c786a
mohammed-rurk <mtj6611@gmail.com>
2026-06-16 14:47:22 +0300

    feats(Student Grades): complete and test

--- 01-CSBasics/Statements/Program.cs
+++ 01-CSBasics/Statements/Program.cs
@@ -10,10 +10,42 @@
              * control statements (conditions, loops, jumb, and exception handling)
              */
 
+            /* C# precedance of operators (from highest to lowest):
+             * 0. member access (.), method call (), and array indexing [] operators
+             * 1. Parentheses ()
+             * 2. Unary operators (++, --, (signs + and -), !)
+             * 3. Multiplicative operators (*, /, %)
+             * 4. Additive operators (+, -)
+             * 5. Relational operators (<, >, <=, >=)
+             * 6. Equality operators (==, !=)
+             * x. &
+             * x. ^
+             * x. | (bitwise operators, opposite is true if both are true and false if both are false)
+             * 7. Logical AND (&&)
+             * 8. Logical OR (||)
+             * 9. Assignment operators (=, +=, -=, etc.)
+             */
+
+            /*
+             * sizeof(double)
+             * typeof(Program)
+             * 
+             */
+            Console.WriteLine("Size of double is: " + sizeof(double));
+            Console.WriteLine("type of Console is: " + typeof(Console));
+            object obj = 4;
+            bool isInt = obj is int; // true, because obj is an int
+            bool isLong = obj is long;
+
+            //as only with referance types eccept if you make the value type nullable.
+            // as if it couldn't convert then it return null without exception
+            string str = obj as string;
             // relational operators return boolean values (true or false) and include : ==, !=, >, <, >=, <=.
 
             //ArthmeticOperators();
-            RelationalOperators();
+            //RelationalOperators();
+            //LogicalOperators();
+            StudentGrades();
 
         }
 
@@ -38,6 +70,67 @@
             Console.WriteLine("5 >= 10 is " + (5 >= 10));
             Console.WriteLine("5 == 10 is " + (5 == 10));
             Console.WriteLine("5 != 10 is " + (5 != 10));
+        }
+
+        static void LogicalOperators()
+        {
+            // logical operators include: && (logical AND), || (logical OR), ! (logical NOT), ^ (logical XOR, opposite is true and same is false).
+            bool a = true;
+            bool b = false;
+            Console.WriteLine($"a && b: {a && b}"); // false
+            Console.WriteLine($"a || b: {a || b}"); // true
+            Console.WriteLine($"a || b: {a ^ b}"); // true n
+            Console.WriteLine($"!a: {!a}"); // false
+            Console.WriteLine($"!b: {!b}"); // true
+
+
+            // && and || are better than | and & as they shortened the evaluation which help in performance.
+        }
+
+        static void StudentGrades() {
+
+            Console.WriteLine("Enter your grade:");
+            int grade = Convert.ToInt32( Console.ReadLine() );
+            if (grade >= 95 && grade <= 100)
+            {
+                Console.WriteLine("You got A+");
+            }
+            else if (grade >= 90 && grade < 95) {
+                Console.WriteLine("You got A");
+            }
+            else if (grade >= 85 && grade < 90)
+            {
+                Console.WriteLine("You got B+");
+            }
+            else if (grade >= 80 && grade < 85)
+            {
+                Console.WriteLine("You got B");
+            }
+            else if (grade >= 75 && grade < 80)
+            {
+                Console.WriteLine("You got C+");
+            }
+            else if (grade >= 70 && grade < 75)
+            {
+                Console.WriteLine("You got C");
+            }
+            else if (grade >= 65 && grade < 70)
+            {
+                Console.WriteLine("You got D+");
+            }
+            else if (grade >= 60 && grade < 65)
+            {
+                Console.WriteLine("You got D");
+            }
+            else if (grade >= 0 && grade < 60)
+            {
+                Console.WriteLine("You got F");
+            }
+            else
+            {
+                Console.WriteLine("Grade value should be between 0 and 100");
+
+            }
         }
     }
 }
